431769d9f3b2bf5499fe37b101e0f250a7d39cc1
Rabindra Dahal <rabindra.dahal@compliancequest.com>
2023-01-27 22:49:04 +0545

    Initiated the project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ProfileCard from './ProfileCard';
+import 'bulma/css/bulma.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>Personal Digital Assistants</div>
+
+      <div className='container'>
+        <div className='section'>
+          <div className='columns'>
+            <div className='column is-3'>
+              <ProfileCard title="Alexa" handle="@alexa99" image="https://cdn.nerdyelectronics.com/wp-content/uploads/2019/10/amazon-alexa-transparent-logo.png" />
+            </div>
+            <div className='column is-3'>
+              <ProfileCard title="Cortana" handle="@cortana" image="https://upload.wikimedia.org/wikipedia/commons/7/7a/Siri_Logo_in_2022.png?20220513194230" />
+            </div>
+            <div className='column is-3'>
+              <ProfileCard title="Siri" handle="@siri01" image="https://e1.pngegg.com/pngimages/758/266/png-clipart-simply-styled-icon-set-731-icons-free-cortana-round-blue-and-teal-logo-thumbnail.png" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
